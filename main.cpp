8473b6d15a761158a2903a1fede71259c3bdd1cb
Tino <tinomuzambi@gmail.com>
2020-05-26 14:37:57 +0200

    Implements reward function.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@ double P() {
     return 1.0;
 }
 
+int R(const string& s, char a, const string& s2) {
+    if (s == "s2" && s2 == "s3") {
+        return 50;
+    }
+    else if (s == "s6" && s2 == "s3") {
+        return 100;
+    }
+    return 0;
+}
+
 int main() {
 
 
