a26fa918681afbbafe46c25466a7d17d3896ff9c
Tino <tinomuzambi@gmail.com>
2020-05-27 01:29:02 +0200

    Adds new class for v_iteration.

---
+++ v_iteration.cpp
@@ -0,0 +1,3 @@
+// MZMTIN002
+
+#include "v_iteration.h"
---
+++ v_iteration.h
@@ -0,0 +1,11 @@
+// MZMTIN002
+
+#ifndef ASSIGNMENT_6_V_ITERATION_H
+#define ASSIGNMENT_6_V_ITERATION_H
+
+namespace MZMTIN002 {
+    class v_iteration {
+
+    };
+}
+#endif //ASSIGNMENT_6_V_ITERATION_H
