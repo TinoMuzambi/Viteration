59f739c574327e848670d1c2347ed05e768d3266
Tino <tinomuzambi@gmail.com>
2020-05-26 14:42:17 +0200

    Initialises all values according to spec.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 // MZMTIN002
 
 #include <string>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
 
 double P() {
@@ -18,7 +21,21 @@ int R(const string& s, char a, const string& s2) {
 }
 
 int main() {
+    unordered_map<string, int> values; // Initialising
+    values["s1"] = 0;
+    values["s2"] = 0;
+    values["s3"] = 0;
+    values["s4"] = 0;
+    values["s5"] = 0;
+    values["s6"] = 0;
 
+    vector<char> actions;
+    actions.push_back('L');
+    actions.push_back('R');
+    actions.push_back('U');
+    actions.push_back('D');
+
+    double GAMMA = 0.8;
 
     return 0;
 }
